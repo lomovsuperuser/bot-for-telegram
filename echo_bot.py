88aaa7a72856f28fe24a421bb45d6d7cef5e1d63
Konstantin Lomov <79058995061@yandex.ru>
2022-05-23 19:57:54 +0500

    echobot is ready

--- echo_bot.py
+++ echo_bot.py
@@ -1,2 +1,15 @@
 #simple echo bot
 import telebot
+
+#
+bot = telebot.TeleBot('5130328485:AAH3afeOqvZAXHvgoNeC2zstEQRdbEuAaNk')
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Online')
+
+@bot.message_handler(content_types=["text"])
+def handler_text(message):
+    bot.send_message(message.chat.id, 'echo: ' + message.text)
+
+bot.polling(none_stop=True, interval=1)
